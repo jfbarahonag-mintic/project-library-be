8bbed6f7cee26c85f92f600c469c35da07847dfa
Juan Felipe Barahona Gonzalez <jfbarahonag@unal.edu.co>
2021-10-10 23:12:43 -0500

    se agrego la ruta /books

--- index.js
+++ index.js
@@ -7,6 +7,28 @@ app.get('/', (req, res) => {
 	res.send('Hello, world!');
 })
 
+app.get('/books', (req, res) => {
+	res.json([{
+			ISBN: '1',
+			title: 'Sherlock Holmes',
+			author: 'Arthur Conan Doyle',
+			price: 10000
+		},
+		{
+			ISBN: '2',
+			title: 'El olvido que seremos',
+			author: 'Hector Abad',
+			price: 20000
+		},
+		{
+			ISBN: '3',
+			title: 'Akelarre',
+			author: 'Mario Mendoza',
+			price: 20000
+		},
+	]);
+})
+
 app.listen(port, () => {
 	console.log(`server listening at ${port}`);
 });
