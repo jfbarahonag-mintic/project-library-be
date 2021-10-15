1a2202481d851d6f4b7be8e122e5d0240b4a8899
Juan Felipe Barahona Gonzalez <jfbarahonag@unal.edu.co>
2021-10-15 18:03:25 -0500

    se connfiguro una variable global para el path usando el sistema

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ connect().catch(err => console.log(err))
 
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //express native middleware
 app.use(express.json())
